5e69b4b50f85d872ab7941d7e4fc21a5b24501b6
Jeremy Burman <jburman@gmail.com>
2020-05-29 16:05:28 -0400

    initial commit

---
+++ Shared/LoadFile.cs
@@ -0,0 +1,12 @@
+﻿
+using W8lessLabs.Blazor.LocalFiles;
+
+namespace BlazorLocalFilesExample
+{
+    public class LoadFile
+    {
+        public SelectedFile SelectedFile { get; set; }
+        public bool IsLoaded { get; set; }
+        public int LoadedSize { get; set; }
+    }
+}
